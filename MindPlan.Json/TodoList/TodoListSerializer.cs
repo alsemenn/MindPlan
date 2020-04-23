5af957e628f1dca07a6fde619bf23003dbb04610
alsemenn <alsemenn@microsoft.com>
2020-04-23 13:03:42 -0400

    Add json serialization

---
+++ MindPlan.Json/TodoList/TodoListSerializer.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using MindPlan.Shared.TodoList;
+using Newtonsoft.Json;
+
+namespace MindPlan.Json.TodoList
+{
+    public class TodoListSerializer
+    {
+        public Shared.TodoList.TodoList Deserialize(string str)
+        {
+            return JsonConvert.DeserializeObject<TodoListJson>(str).Create();
+        }
+
+        public string Serialize(Shared.TodoList.TodoList todoList)
+        {
+            return JsonConvert.SerializeObject(new TodoListJson(todoList));
+        }
+
+        [JsonObject(MemberSerialization.OptIn)]
+        private class TodoListJson
+        {
+            [JsonProperty]
+            public List<TodoListItemJson> Items { get; set; }
+
+            [JsonProperty]
+            public Guid Id { get; set; }
+
+            public Shared.TodoList.TodoList Create()
+            {
+                return new Shared.TodoList.TodoList(Id, Items.Select(i => i.Create()).ToList());
+            }
+
+            public TodoListJson()
+            {
+            }
+
+            public TodoListJson(Shared.TodoList.TodoList todoList)
+            {
+                Items = todoList.Items.Select(i => new TodoListItemJson(i)).ToList();
+                Id = todoList.Id;
+            }
+        }
+
+        [JsonObject(MemberSerialization.OptIn)]
+        private class TodoListItemJson
+        {
+            [JsonProperty]
+            public Guid Id { get; set; }
+
+            [JsonProperty]
+            public string Text { get; set; }
+
+            public TodoListItem Create()
+            {
+                return new TodoListItem(Id) { Text = Text };
+            }
+
+            public TodoListItemJson()
+            {
+            }
+
+            public TodoListItemJson(TodoListItem item)
+            {
+                Id = item.Id;
+                Text = item.Text;
+            }
+        }
+    }
+}
---
+++ MindPlan.Shared/TodoList/TodoListItem.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MindPlan.Shared.TodoList
+{
+    public class TodoListItem: IEquatable<TodoListItem>
+    {
+        public Guid Id { get; }
+
+        public string Text { get; set; }
+
+        public TodoListItem(Guid id)
+        {
+            Text = string.Empty;
+            Id = id;
+        }
+
+        public bool Equals(TodoListItem? other)
+        {
+            if (other == null)
+            {
+                return false;
+            }
+            return (Id == other.Id) && (Text == other.Text);
+        }
+
+        public override bool Equals(object obj)
+        {
+            return Equals(obj as TodoListItem);
+        }
+
+        public override int GetHashCode()
+        {
+            return Id.GetHashCode();
+        }
+    }
+}
